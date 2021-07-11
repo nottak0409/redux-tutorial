4b7ba2a0af20633130478de603dcb387c54c20dc
nottak0409 <nottak0409@gmail.com>
2021-07-11 14:28:14 +0900

    reducerの定義追加

--- src/reducer.js
+++ src/reducer.js
@@ -1,23 +1,43 @@
 const initialState = {
-  todos: [
-    { id: 0, text: 'Learn React', completed: true },
-    { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
-    { id: 2, text: 'Build something fun!', completed: false, color: 'blue' }
-  ],
-  filters: {
-    status: 'All',
-    colors: []
-  }
+    todos: [
+        { id: 0, text: 'Learn React', completed: true },
+        { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
+        { id: 2, text: 'Build something fun!', completed: false, color: 'blue' }
+    ],
+    filters: {
+        status: 'All',
+        colors: []
+    }
+}
+
+function nextTodoId(todos) {
+    const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
+    return maxId + 1
 }
 
 // Use the initialState as a default value
 export default function appReducer(state = initialState, action) {
-  // The reducer normally looks at the action type field to decide what happens
-  switch (action.type) {
-    // Do something here based on the different types of actions
-    default:
-      // If this reducer doesn't recognize the action type, or doesn't
-      // care about this specific action, return the existing state unchanged
-      return state
-  }
+    // The reducer normally looks at the action type field to decide what happens
+    switch (action.type) {
+        case 'todos/todoAdded': {
+            return {
+                //現在のstateを展開
+                ...state,
+                todos: [
+                    //今までのstateの後に新しいtodoをpush
+                    ...state.todos,
+                    {
+                        id: nextTodoId(state.todos),
+                        text: action.payload,
+                        completed: false
+                    }
+                ]
+            }
+        }
+        // Do something here based on the different types of actions
+        default:
+        // If this reducer doesn't recognize the action type, or doesn't
+        // care about this specific action, return the existing state unchanged
+        return state
+    }
 }
