f48104c1150a487f24f79cac9c101a42973222f7
nottak0409 <nottak0409@gmail.com>
2021-07-11 15:46:37 +0900

    rootreducer 作成 ”

--- src/reducer.js
+++ src/reducer.js
@@ -1,69 +1,9 @@
-const initialState = {
-    todos: [
-        { id: 0, text: 'Learn React', completed: true },
-        { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
-        { id: 2, text: 'Build something fun!', completed: false, color: 'blue' }
-    ],
-    filters: {
-        status: 'All',
-        colors: []
-    }
-}
+import todosReducer from './features/todos/todosSlice'
+import filtersReducer from './features/filters/filtersSlice'
 
-function nextTodoId(todos) {
-    const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
-    return maxId + 1
-}
-
-// Use the initialState as a default value
-export default function appReducer(state = initialState, action) {
-    // The reducer normally looks at the action type field to decide what happens
-    switch (action.type) {
-        case 'todos/todoAdded': {
-            return {
-                //現在のstateを展開
-                ...state,
-                todos: [
-                    //今までのstateの後に新しいtodoをpush
-                    ...state.todos,
-                    {
-                        id: nextTodoId(state.todos),
-                        text: action.payload,
-                        completed: false
-                    }
-                ]
-            }
-        }
-        case 'todos/todoToggled': {
-            return {
-                ...state,
-                todos: state.todos.map(todo => {
-                    //idが存在しない値の時
-                    if (todo.id !== action.payload) {
-                        return todo
-                    }
-                    //todoが変化した時
-                    return {
-                        ...todo,
-                        completed: !todo.completed
-                    }
-                })
-            }
-        }
-        case 'filters/statusFilterChanged': {
-            return {
-                ...state,
-                //statusフィルターを変更する
-                filters: {
-                    ...state.filters,
-                    status: action.payload
-                }
-            }
-        }
-        // Do something here based on the different types of actions
-        default:
-        // If this reducer doesn't recognize the action type, or doesn't
-        // care about this specific action, return the existing state unchanged
-        return state
+export default function rootReducer(state = {}, action) {
+    return {
+        todos: todosReducer(state.todos, action),
+        filters: filtersReducer(state.filters, action)
     }
 }
