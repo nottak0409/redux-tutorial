22e8fc1b93d84675273c167b2a364e3b599435d8
nottak0409 <nottak0409@gmail.com>
2021-07-14 07:52:17 +0900

    change top page

--- src/index.js
+++ src/index.js
@@ -1,43 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
 import store from './store'
-import './api/server'
 
-console.log('initial state: ', store.getState())
-
-const unsubscribe = store.subscribe(() =>
-  console.log('State after dispatch: ', store.getState())
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 )
-
-// Now, dispatch some actions
-
-store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
-store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about reducers' })
-store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about stores' })
-
-store.dispatch({ type: 'todos/todoToggled', payload: 0 })
-store.dispatch({ type: 'todos/todoToggled', payload: 1 })
-
-store.dispatch({ type: 'filters/statusFilterChanged', payload: 'Active' })
-
-store.dispatch({
-  type: 'filters/colorFilterChanged',
-  payload: { color: 'red', changeType: 'added' }
-})
-
-// Stop listening to state updates
-unsubscribe()
-
-// Dispatch one more action to see what happens
-store.dispatch({ type: 'todos/todoAdded', payload: 'Try creating a store' })
-
-// Omit existing React rendering logic
-
-//ReactDOM.render(
- // <React.StrictMode>
-//    <App />
- // </React.StrictMode>,
-  //document.getElementById('root')
-//)
