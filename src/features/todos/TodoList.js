c263c6bacaac3b37d331ce9fd40b3d1e0ad910fa
nottak0409 <nottak0409@gmail.com>
2021-07-14 08:19:25 +0900

    change color

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -2,17 +2,16 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import TodoListItem from './TodoListItem'
 
-const SelectTodos = state => state.todos
+const selectTodos = (state) => state.todos
 
 const TodoList = () => {
-    const todos = useSelector(SelectTodos)
+  const todos = useSelector(selectTodos)
 
-    console.log(todos);
-    const renderedListItems = todos.map(todo => {
-        return <TodoListItem key={todo.id} todo={todo} />
-    })
+  const renderedListItems = todos.map((todo) => {
+    return <TodoListItem key={todo.id} todo={todo} />
+  })
 
-    return <ul className="todo-list">{renderedListItems}</ul>
+  return <ul className="todo-list">{renderedListItems}</ul>
 }
 
 export default TodoList
