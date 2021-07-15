6f357b459471397e8be17c35f0d53e5bec216415
nottak0409 <nottak0409@gmail.com>
2021-07-16 07:26:28 +0900

    delete実装

--- src/features/todos/TodoListItem.js
+++ src/features/todos/TodoListItem.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 import { ReactComponent as TimesSolid } from './times-solid.svg'
 
 import { availableColors, capitalize } from '../filters/colors'
 
-const TodoListItem = ({ todo, onColorChange, onCompletedChange, onDelete }) => {
+const selectTodoById = (state, todoId) => {
+    return state.todos.find((todo) => todo.id === todoId)
+}
+const TodoListItem = ({ id, onColorChange, onCompletedChange }) => {
+    const todo = useSelector((state) => selectTodoById(state, id))
     const { text, completed, color } = todo
+    const dispatch = useDispatch()
 
     const handleCompletedChanged = (e) => {
         onCompletedChange(e.target.checked)
@@ -20,6 +26,10 @@ const TodoListItem = ({ todo, onColorChange, onCompletedChange, onDelete }) => {
         {capitalize(c)}
         </option>
     ))
+
+    const onDelete = (e) => {
+        dispatch({ type: 'todos/todoDeleted', payload: id })
+    }
 
     return (
         <li>
