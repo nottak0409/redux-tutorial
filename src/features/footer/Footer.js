8bda4feb844439e01a82ce29683c8448cebb53ee
nottak0409 <nottak0409@gmail.com>
2021-07-16 11:09:58 +0900

    all completed 実装

--- src/features/footer/Footer.js
+++ src/features/footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 import { availableColors, capitalize } from '../filters/colors'
 import { StatusFilters } from '../filters/filtersSlice'
