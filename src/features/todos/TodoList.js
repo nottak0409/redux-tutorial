588c792bac46d4590d415430099e2092b7c283a0
nottak0409 <nottak0409@gmail.com>
2021-07-18 14:17:38 +0900

    todos change

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,16 +1,26 @@
 import React from 'react'
-import { useSelector, shallowEqual } from 'react-redux'
+import { useSelector } from 'react-redux'
 import TodoListItem from './TodoListItem'
-import { selectTodoIds } from './todosSlice'
+
+import { selectFilteredTodoIds } from './todosSlice'
 
 const TodoList = () => {
-  const todoIds = useSelector(selectTodoIds, shallowEqual)
+    const todoIds = useSelector(selectFilteredTodoIds)
+    const loadingStatus = useSelector((state) => state.todos.status)
 
-  const renderedListItems = todoIds.map(todoId => {
-    return <TodoListItem key={todoId} id={todoId} />
-  })
+    if (loadingStatus === 'loading') {
+        return (
+            <div className="todo-list">
+                <div className="loader" />
+            </div>
+        )
+    }
 
-  return <ul className="todo-list">{renderedListItems}</ul>
+    const renderedListItems = todoIds.map((todoId) => {
+        return <TodoListItem key={todoId} id={todoId} />
+    })
+
+    return <ul className="todo-list">{renderedListItems}</ul>
 }
 
 export default TodoList
--- src/features/todos/TodoListItem.js
+++ src/features/todos/TodoListItem.js
@@ -4,61 +4,67 @@ import { useSelector, useDispatch } from 'react-redux'
 import { ReactComponent as TimesSolid } from './times-solid.svg'
 
 import { availableColors, capitalize } from '../filters/colors'
+import {
+    todoColorSelected,
+    todoDeleted,
+    todoToggled,
+    selectTodoById,
+} from './todosSlice'
 
-const selectTodoById = (state, todoId) => {
-    return state.todos.entities.find((todo) => todo.id === todoId)
-}
+// Destructure `props.id`, since we just need the ID value
 const TodoListItem = ({ id }) => {
+    // Call our `selectTodoById` with the state _and_ the ID value
     const todo = useSelector((state) => selectTodoById(state, id))
     const { text, completed, color } = todo
+    
     const dispatch = useDispatch()
 
-    const handleCompletedChanged = (e) => {
-        dispatch({ type: 'todos/todoToggled', payload: todo.id})
+    const handleCompletedChanged = () => {
+        dispatch(todoToggled(todo.id))
     }
 
     const handleColorChanged = (e) => {
         const color = e.target.value
-        dispatch({ type: 'todos/colorSelected', payload: { todoId: todo.id, color } })
+        dispatch(todoColorSelected(todo.id, color))
+    }
+
+    const onDelete = () => {
+        dispatch(todoDeleted(todo.id))
     }
 
     const colorOptions = availableColors.map((c) => (
         <option key={c} value={c}>
-        {capitalize(c)}
+            {capitalize(c)}
         </option>
     ))
 
-    const onDelete = (e) => {
-        dispatch({ type: 'todos/todoDeleted', payload: todo.id })
-    }
-
     return (
         <li>
-        <div className="view">
-        <div className="segment label">
-        <input
-        className="toggle"
-        type="checkbox"
-        checked={completed}
-        onChange={handleCompletedChanged}
-        />
-        <div className="todo-text">{text}</div>
-        </div>
-        <div className="segment buttons">
-        <select
-        className="colorPicker"
-        value={color}
-        style={{ color }}
-        onChange={handleColorChanged}
-        >
-        <option value=""></option>
-        {colorOptions}
-        </select>
-        <button className="destroy" onClick={onDelete}>
-        <TimesSolid />
-        </button>
-        </div>
-        </div>
+            <div className="view">
+                <div className="segment label">
+                    <input
+                        className="toggle"
+                        type="checkbox"
+                        checked={completed}
+                        onChange={handleCompletedChanged}
+                        />
+                    <div className="todo-text">{text}</div>
+                </div>
+                <div className="segment buttons">
+                    <select
+                        className="colorPicker"
+                        value={color}
+                        style={{ color }}
+                        onChange={handleColorChanged}
+                        >
+                        <option value=""></option>
+                        {colorOptions}
+                    </select>
+                    <button className="destroy" onClick={onDelete}>
+                        <TimesSolid />
+                    </button>
+                </div>
+            </div>
         </li>
     )
 }
