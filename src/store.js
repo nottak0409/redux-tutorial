7b69df3d2c4582caee9650e1cbcc8947f1922362
nottak0409 <nottak0409@gmail.com>
2021-07-18 17:49:14 +0900

    redux-toolkit利用

--- src/store.js
+++ src/store.js
@@ -1,9 +1,20 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import rootReducer from './reducer'
+//import { createStore, applyMiddleware } from 'redux'
+//import thunkMiddleware from 'redux-thunk'
+//import { composeWithDevTools } from 'redux-devtools-extension'
+//import rootReducer from './reducer'
+//コメントアウトしているコードはtoolskitを利用する前のコード
+import { configureStore } from '@reduxjs/toolkit'
+import todosReducer from './features/todos/todosSlice'
+import filtersReducer from './features/filters/filtersSlice'
 
-const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
-const store = createStore(rootReducer, composedEnhancer)
+
+//const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
+//const store = createStore(rootReducer, composedEnhancer)
+const store = configureStore({
+    reducer: {
+        todos: todosReducer,
+        filters: filtersReducer
+    }
+})
 
 export default store
